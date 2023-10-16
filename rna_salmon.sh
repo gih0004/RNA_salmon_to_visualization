b1e2ea2f95af00e67cab6c2c9854e134850d9e38
Guillian Hernandez <gih0004@vt.edu>
2023-10-16 13:42:01 -0400

    rna_salmon.sh

--- rna_salmon.sh
+++ rna_salmon.sh
@@ -1,14 +1,16 @@
 module load salmon
 
 # make index using transcriptome fasta (used high confidence model)
-salmon index -t ATL_v3.hc_gene_models.repr.cdna.fa.gz -i salmon_ATL.v3.index
+
 mkdir salmon_quant
+
+salmon index -t {species reference cdna file} -i {desired_index_name}.index
+
 for fq1 in ./*.bam #<>This should be changed into whichever string you have last in sample names common between all samples 
 do
-
-    Seq="${fq1%.bam}"
-
-    salmon quant -i salmon_ATL.v3.index -l A -1 ${Seq}.filtered_1.fq -2 ${Seq}.filtered_2.fq --gcBias -o ./salmon/${Seq}
+    base="${fq1%.bam}"
+#Generate transcript expression quantification files for each sample 
+    salmon quant -i salmon_ATL.v3.index -l A -1 ${base}.filtered_1.fq -2 ${base}.filtered_2.fq --gcBias -o ./salmon/${base}
 
 
 done
